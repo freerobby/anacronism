40237ffbe5db8fa166a5b3ecfa1fd0ce56681a15
Robby Grossman <robby@freerobby.com>
2011-08-02 13:23:24 -0400

    Send max of 3 consecutive emails.

--- app.rb
+++ app.rb
@@ -7,16 +7,21 @@ use Rack::Auth::Basic, "Access Restricted" do |username, password|
 end
 
 get '/' do
+  consecutive_emails = ENV["CONSECUTIVE_EMAILS"].to_i
   last_ping = nil
   last_ping = Time.at(ENV["LAST_PING_RECEIVED_AT"].to_i).to_i if !ENV["LAST_PING_RECEIVED_AT"].nil? && ENV["LAST_PING_RECEIVED_AT"].length > 0
   this_ping = Time.now.to_i
-  if !last_ping.nil? && (this_ping - last_ping > ENV["MAX_INTERVAL"].to_i)
+  if !last_ping.nil? && (this_ping - last_ping > ENV["MAX_INTERVAL"].to_i) && consecutive_emails.to_i < 3
+    consecutive_emails += 1
     Pony.mail :to => ENV["NOTIFY_LIST"],
               :from => ENV["EMAIL_FROM"],
               :subject => "[Anacronism] Cron Interval Exceeded #{ENV["MAX_INTERVAL"]} Seconds",
-              :body => "The last cron interval was #{this_ping - last_ping} seconds."
+              :body => "The last cron interval was #{this_ping - last_ping} seconds.#{' This is the third and final notice.' if consecutive_emails == 3}"
+  elsif last_ping.nil? || (this_ping - last_ping <= ENV["MAX_INTERVAL"].to_i)
+    consecutive_emails = 0
   end
   
   ENV["LAST_PING_RECEIVED_AT"] = this_ping.to_s
+  ENV["CONSECUTIVE_EMAILS"] = consecutive_emails.to_s
   "ACK"
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -99,5 +99,67 @@ describe "Anacronism App" do
       ENV["MAX_INTERVAL"] = old_max_interval
       ENV["LAST_PING_RECEIVED_AT"] = old_lpra
     end
+    
+    it "sends max of 3 consecutive emails" do
+      old_lpra = ENV["LAST_PING_RECEIVED_AT"]
+      old_max_interval = ENV["MAX_INTERVAL"]
+      old_consecutive_emails = ENV["CONSECUTIVE_EMAILS"]
+      
+      ENV["CONSECUTIVE_EMAILS"] = ""
+      ENV["LAST_PING_RECEIVED_AT"] = "1312299572"
+      ENV["MAX_INTERVAL"] = "30"
+      
+      Timecop.freeze(Time.at(1312299603)) # 31 seconds later
+      Pony.should_receive(:mail)
+      get '/'
+      Timecop.return
+      
+      Timecop.freeze(Time.at(1312299634)) # 62 seconds later
+      Pony.should_receive(:mail)
+      get '/'
+      Timecop.return
+      
+      Timecop.freeze(Time.at(1312299665)) # 93 seconds later
+      Pony.should_receive(:mail)
+      get '/'
+      Timecop.return
+      
+      Timecop.freeze(Time.at(1312299696)) # 124 seconds later
+      Pony.should_not_receive(:mail)
+      get '/'
+      Timecop.return
+      
+      ENV["MAX_INTERVAL"] = old_max_interval
+      ENV["LAST_PING_RECEIVED_AT"] = old_lpra
+      ENV["CONSECUTIVE_EMAILS"] = old_consecutive_emails
+    end
+    it "resets consecutive emails when within interval" do
+      old_lpra = ENV["LAST_PING_RECEIVED_AT"]
+      old_max_interval = ENV["MAX_INTERVAL"]
+      old_consecutive_emails = ENV["CONSECUTIVE_EMAILS"]
+      
+      ENV["CONSECUTIVE_EMAILS"] = ""
+      ENV["LAST_PING_RECEIVED_AT"] = "1312299572"
+      ENV["MAX_INTERVAL"] = "30"
+      
+      Timecop.freeze(Time.at(1312299603)) # 31 seconds later
+      Pony.should_receive(:mail)
+      get '/'
+      Timecop.return
+      
+      Timecop.freeze(Time.at(1312299634)) # 62 seconds later
+      Pony.should_receive(:mail)
+      get '/'
+      Timecop.return
+      
+      Timecop.freeze(Time.at(1312299663)) # 91 seconds later
+      get '/'
+      ENV["CONSECUTIVE_EMAILS"].should == "0"
+      Timecop.return
+      
+      ENV["MAX_INTERVAL"] = old_max_interval
+      ENV["LAST_PING_RECEIVED_AT"] = old_lpra
+      ENV["CONSECUTIVE_EMAILS"] = old_consecutive_emails
+    end
   end
 end
